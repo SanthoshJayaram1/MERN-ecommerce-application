e83aa6d54c99bdfe157989331b396068f3d10706
akulajayaram <akhulaghanasai@ascent-hr.com>
2024-12-01 16:38:50 +0530

    fixes in deployment

--- client/src/config.js
+++ client/src/config.js
@@ -4,5 +4,6 @@ axios.defaults.withCredentials = true;
 export const axiosInstance = axios.create({
     // baseURL:"https://ecomshoppify.onrender.com/",
     // baseURL: "http://localhost:5000/",
-    baseURL:"http://ec2-65-1-95-35.ap-south-1.compute.amazonaws.com/"
+    // baseURL: "http://ec2-65-1-95-35.ap-south-1.compute.amazonaws.com/",
+    baseURL:"http://ec2-13-201-81-247.ap-south-1.compute.amazonaws.com/"
 });
